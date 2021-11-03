72fd480c9717cbdc8a99829d920d3ddaab80b330
Distrotion <sittichok.ho@gmail.com>
2021-11-03 17:01:17 +0700

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "59d175b51a8dffb1c2fb51fcda65d0f4",
-"index.html": "66abafc718b7f162fb074a5bee909b23",
-"/": "66abafc718b7f162fb074a5bee909b23",
-"main.dart.js": "c4ea10f38b722616ddbd57d78985df39",
+"index.html": "7616b6105bb9db1b9aa645d0459caef9",
+"/": "7616b6105bb9db1b9aa645d0459caef9",
+"main.dart.js": "b1d2a04adb00c5c0264840e1a11457f8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
